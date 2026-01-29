e5c531a4d9ebfe3f9df18769a410b750183482f1
ahsan.fasihi <ahsan.fasihi@PC-3384>
2026-01-29 11:45:40 +0500

    alter userid column

--- CancerScreening.Domain/Entities/CancerAssessment.cs
+++ CancerScreening.Domain/Entities/CancerAssessment.cs
@@ -1,12 +1,16 @@
-namespace CancerScreening.Domain.Entities 
-
+namespace CancerScreening.Domain.Entities
 {
     public class CancerAssessment
     {
         public int Id { get; set; }
-        public string UserId { get; set; } = string.Empty;
+
+        // Nullable for anonymous users
+        public string? UserId { get; set; }
         public string CancerType { get; set; } = string.Empty;
         public int Score { get; set; }
         public string RiskLevel { get; set; } = string.Empty;
+
+        // Optional but useful
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     }
 }
--- CancerScreening.Persistence/Migrations/20260129064309_alterCancerAssessmentTbl.Designer.cs
+++ CancerScreening.Persistence/Migrations/20260129064309_alterCancerAssessmentTbl.Designer.cs
@@ -4,6 +4,7 @@ using CancerScreening.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CancerScreening.Persistence.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260129064309_alterCancerAssessmentTbl")]
+    partial class alterCancerAssessmentTbl
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
